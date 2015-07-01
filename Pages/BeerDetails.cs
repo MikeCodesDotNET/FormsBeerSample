a231021ff846f944a2b12bd21a263187f078ac71
Mike James <mike@xamarin.com>
2015-07-01 12:43:42 +0100

    Initial commit

---
+++ Pages/BeerDetails.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FormsBeerSample.Pages
+{
+    public class BeerDetails : ContentPage
+    {
+        Label name;
+        Label description;
+        Image image;
+
+        public BeerDetails(BreweryDB.Models.Beer beer)
+        {
+            Title = beer.Name;
+
+            name = new Label
+            {
+                Text = beer.Name,
+                FontSize = 20,
+            };
+
+            description = new Label
+            {
+                Text = beer.Description,
+                FontSize = 10
+            };
+
+            image = new Image
+            {
+                Source = beer.Labels.Medium
+            };
+
+            Content = new ScrollView
+            {
+                    Content = new StackLayout
+                    {
+                        Padding = 10,
+                        Children = {image, name, description}
+                    }
+            };
+        }
+    }
+}
+
---
+++ Pages/SearchBeersPage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using Xamarin.Forms;
+using BreweryDB.Models;
+
+namespace FormsBeerSample.Pages
+{
+    public class SearchBeersPage : ContentPage
+    {
+        SearchBar searchBar;
+        ListView listView;
+
+        public SearchBeersPage()
+        {
+
+            Title = "Search";
+
+            //TODO Add your BreweryDB Key 
+            BreweryDB.BreweryDBClient.Initialize("", "SampleApp", 1);
+
+            listView = new ListView();
+
+            var cell = new DataTemplate(typeof(TextCell));
+            cell.SetBinding(TextCell.TextProperty, "Name");
+            cell.SetBinding(TextCell.DetailProperty, "Description");
+
+            listView.ItemTemplate = cell;
+
+            listView.ItemSelected += delegate
+            {
+                var beer = listView.SelectedItem as Beer;
+                if(beer == null)
+                    return;
+                Navigation.PushAsync(new BeerDetails(beer));
+                listView.SelectedItem = null;
+            };
+
+            searchBar = new SearchBar
+            {
+                Placeholder = "Search for beer"
+            };
+
+            searchBar.SearchButtonPressed += async delegate
+            {
+               var results =  await new BreweryDB.BreweryDBSearch<Beer>(searchBar.Text).Search();
+               listView.ItemsSource = results;
+            };
+
+            Content = new StackLayout
+            {
+                Children = {searchBar, listView}
+            };
+        }
+    }
+}
+
